8ebd1b8b587e35b210365d944fdb458eabeac9cd
Joyce Chan <joycehwchan@Joyces-MacBook-Pro.local>
2022-12-08 01:53:39 +0900

    remove links from footer

--- src/compoents/footer/Footer.jsx
+++ src/compoents/footer/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
         <div className="footer__container container">
             {/* <h1 className="footer__title">Joyce C.</h1> */}
 
-            <ul className="footer__list">
+            {/* <ul className="footer__list">
                 <li>
                     <a href="#about" className="footer__link">About</a>
                 </li>
@@ -19,7 +19,7 @@ const Footer = () => {
                 <li>
                     <a href="#testimonials" className="footer__link">Testimonials</a>
                 </li>
-            </ul>
+            </ul> */}
 
             <div className="footer__social">
                 <a href="https://www.linkedin.com/in/joycehwchan/" className="footer__social-link" target="_blank" rel="noreferrer">
