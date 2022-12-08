20844efd3e2a2a6cfa259d1432c5f3b4b25310c2
Joyce Chan <joycehwchan@Joyces-MacBook-Pro.local>
2022-12-08 16:53:34 +0900

    update portfolio content

--- src/compoents/testimonials/Data.jsx
+++ src/compoents/testimonials/Data.jsx
@@ -6,19 +6,19 @@ export const Data = [
     {
         id: 1,
         image: Image1,
-        title: "Person 1",
-        description: "Great teammate, love working together!"
+        title: "Hafid Qarchi",
+        description: "I had a chance to work with Joyce on our final project in Le wagon, together with other teammates making her amazing idea come true. Very knowledgeable on Front-End and I learned a lot of CSS tricks from her. She will be a great asset to any company as a Front-end developer."
     },
     {
         id: 2,
         image: Image2,
-        title: "Person 2",
-        description: "Great teammate, love working together!"
+        title: "Fred Faeger",
+        description: "..."
     },
     {
         id: 3,
         image: Image3,
-        title: "Person 3",
-        description: "Great teammate, love working together!"
+        title: "Christopher Ikaika Crespo",
+        description: "Joyce's motivation and hard work are unbeatable. She moves forward achieving outstanding results. Highly knowledgeable both in backend and frontend she always keeps an extra card up her sleeve when facing coding issues."
     },
 ]
--- src/compoents/work/Data.jsx
+++ src/compoents/work/Data.jsx
@@ -1,9 +1,6 @@
 import Work1 from "../../assets/work1.jpg"
 import Work2 from "../../assets/work2.jpg"
 import Work3 from "../../assets/work3.jpg"
-import Work4 from "../../assets/work4.jpg"
-import Work5 from "../../assets/work5.jpg"
-import Work6 from "../../assets/work5.jpg"
 
 export const projectsData = [
     {
@@ -32,34 +29,12 @@ export const projectsData = [
         id: 3,
         image: Work3,
         title: 'Portfolio',
-        description: 'My first React website. Version 3.0 of my personal portfolio website.',
-        link: 'https://github.com/joycehwchan',
-        github: 'https://github.com/joycehwchan',
+        description: 'Personal portfolio to showcase my projects.',
+        link: '#',
+        github: 'https://github.com/joycehwchan/portfolio',
         category: 'design',
         role: 'Front-end Developer, UI Designer',
         used: 'Figma, React, CSS'
-    },
-    {
-        id: 4,
-        image: Work4,
-        title: 'Longest Word Game',
-        description: 'Description',
-        link: 'https://github.com/joycehwchan',
-        github: 'https://github.com/joycehwchan',
-        category: 'fun',
-        role: 'Full-stack Developer',
-        used: 'HTML, CSS, Bootstrap, Javascript'
-    },
-    {
-        id: 5,
-        image: Work5,
-        title: 'Wagon Race',
-        description: 'Description',
-        link: 'https://joycehwchan.github.io/lewagon-wagon-race/',
-        github: 'https://github.com/joycehwchan/lewagon-wagon-race',
-        category: 'fun',
-        role: 'Full-stack developer',
-        used: 'HTML, CSS, Javascript'
     },
 ]
 
@@ -67,5 +42,4 @@ export const projectsNav = [
     {name: 'all'},
     {name: 'web'},
     {name: 'design'},
-    {name: 'fun'},
 ]
