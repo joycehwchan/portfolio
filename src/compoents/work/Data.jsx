e0e6ef3bdb1473468093754cfd14678d01b167d2
joycehwchan <joycehwchan@gmail.com>
2022-12-11 19:06:29 +0900

    added new project

--- src/compoents/work/Data.jsx
+++ src/compoents/work/Data.jsx
@@ -1,45 +1,58 @@
-import Work1 from "../../assets/work1.jpg"
-import Work2 from "../../assets/work2.jpg"
-import Work3 from "../../assets/work3.jpg"
+import Work1 from "../../assets/work1.jpg";
+import Work2 from "../../assets/work2.jpg";
+import Work3 from "../../assets/work3.jpg";
+import Work4 from "../../assets/work4.jpg";
 
 export const projectsData = [
-    {
-        id: 1,
-        image: Work1,
-        title: 'TabiNow',
-        description: 'A few clicks to generate and customize travel itineraries.',
-        link: 'https://tabinow.tours/',
-        github: 'https://github.com/joycehwchan/tabinow',
-        category: 'web',
-        role: 'Front-end Lead',
-        used: 'HTML/Sass, Javascript, Ruby on Rails, Devise, Pundit, APIs, Cloudinary, Mapbox, Figma'
-    },
-    {
-        id: 2,
-        image: Work2,
-        title: 'MinaNoka',
-        description: 'Rent a farming field to experience rural Japan.',
-        link: '',
-        github: 'https://github.com/joycehwchan/minanoka',
-        category: 'web',
-        role: 'Lead Developer',
-        used: 'HTML/CSS, Bootstrap, Ruby on Rails, Devise, Cloudinary, Mapbox, Figma'
-    },
-    {
-        id: 3,
-        image: Work3,
-        title: 'Portfolio',
-        description: 'Personal portfolio to showcase my projects.',
-        link: '#',
-        github: 'https://github.com/joycehwchan/portfolio',
-        category: 'design',
-        role: 'Front-end Developer, UI Designer',
-        used: 'Figma, React, CSS'
-    },
-]
+  {
+    id: 1,
+    image: Work1,
+    title: "TabiNow",
+    description: "A few clicks to generate and customize travel itineraries.",
+    link: "https://tabinow.tours/",
+    github: "https://github.com/joycehwchan/tabinow",
+    category: "web",
+    role: "Front-end Lead",
+    used: "HTML/Sass, Javascript, Ruby on Rails, Devise, Pundit, APIs, Cloudinary, Mapbox, Figma",
+  },
+  {
+    id: 2,
+    image: Work2,
+    title: "MinaNoka",
+    description: "Rent a farming field to experience rural Japan.",
+    link: "",
+    github: "https://github.com/joycehwchan/minanoka",
+    category: "web",
+    role: "Lead Developer",
+    used: "HTML/CSS, Bootstrap, Ruby on Rails, Devise, Cloudinary, Mapbox, Figma",
+  },
+  {
+    id: 3,
+    image: Work3,
+    title: "Portfolio",
+    description: "Personal portfolio to showcase my projects.",
+    link: "#",
+    github: "https://github.com/joycehwchan/portfolio",
+    category: "design",
+    role: "Front-end Developer, UI Designer",
+    used: "Figma, React, CSS",
+  },
+  {
+    id: 4,
+    image: Work4,
+    title: "Memory Game",
+    description: "Click on all characters only once!",
+    link: "https://joycehwchan.github.io/memory-game/",
+    github: "https://github.com/joycehwchan/memory-game",
+    category: "fun",
+    role: "Full-stack Developer",
+    used: "React, CSS",
+  },
+];
 
 export const projectsNav = [
-    {name: 'all'},
-    {name: 'web'},
-    {name: 'design'},
-]
+  { name: "all" },
+  { name: "web" },
+  { name: "design" },
+  { name: "fun" },
+];
